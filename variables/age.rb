32bf2bfa4366d40e77b15edc45458256ff4786c7
Fe Geiger <feo.geiger@web.de>
2018-08-16 16:20:57 +0200

    Add more exercises

---
+++ variables/age.rb
@@ -0,0 +1,6 @@
+puts "How old are you?"
+age = gets.chomp
+puts "In ten years you will be " + (age.to_i + 10).to_s
+puts "In twenty years you will be " + (age.to_i + 20).to_s
+puts "In thirty years you will be " + (age.to_i + 30).to_s
+puts "In fifty years you will be " + (age.to_i + 50).to_s
